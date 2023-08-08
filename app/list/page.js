183f4bd753b63dcbdd4d0f50a9c6952a76654821
seongho <damadu01@gmail.com>
2023-08-08 23:46:11 +0900

    Next.js에서 이미지 넣는 법 2개

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>
-        <h4 className='title'>상품목록</h4>
-        <div class="food">
-            <h4>상품1 $40</h4>
+export default function List() {
+    let products = ["Tomatoes", "Pasta", "Coconut"];
+    return (
+        <div>
+            <h4 className="title">상품목록</h4>
+            {products.map((product, idx) => {
+                return (
+                    <div class="food" key={idx}>
+                        <img src={`/food${idx}.png`} alt="토마토" className="food-img" />
+                        <h4>{product} $40</h4>
+                    </div>
+                );
+            })}
         </div>
-        <div class="food">
-            <h4>상품2 $40</h4>
-        </div>
-    </div>
-  )
+    );
 }

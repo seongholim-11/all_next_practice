e3bbc0a98b53f5e7995942a0a397b281932388b4
seongho <damadu01@gmail.com>
2023-08-08 23:13:29 +0900

    여러 페이지 만들기 (라우팅)

---
+++ app/cart/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1>장바구니입니다.</h1>
+    </div>
+  )
+}
---
+++ app/list/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h4 className='title'>상품목록</h4>
+        <div class="food">
+            <h4>상품1 $40</h4>
+        </div>
+        <div class="food">
+            <h4>상품2 $40</h4>
+        </div>
+    </div>
+  )
+}

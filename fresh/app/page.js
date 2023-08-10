20900c458d8f26afbbe4376fe40fe520542d2b02
seongho <damadu01@gmail.com>
2023-08-10 21:19:15 +0900

    fresh

---
+++ fresh/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    let name = "park";
+    return (
+        <div>
+            <h4 className="title" style={{ color: "red", fontSize: "30px" }}>
+                애플후레시
+            </h4>
+            <p class="title-sub">by dev {name}</p>
+        </div>
+    );
+}
